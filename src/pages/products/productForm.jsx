91212c81350cb2599de6d5883123b81ff56df6d7
porawat chinpala <porawat.chi@metthier.co.th>
2025-05-17 20:39:01 +0700

    สร้างสินค้า

--- src/pages/products/productForm.jsx
+++ src/pages/products/productForm.jsx
@@ -2,7 +2,7 @@ const ProductForm = ({ action = "create" }) => {
   console.log("ProductForm action:", action);
   return (
     <div>
-      <h1>Product Form</h1>
+      <h1>Product Form สำหรับสร้างสินค้า</h1>
       {/* Add your form elements here */}
     </div>
   );
