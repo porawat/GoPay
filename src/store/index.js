6696a7e95d17924f06afec1fdcf082d057062816
porawat chinpala <porawat.chi@metthier.co.th>
2025-06-07 00:20:22 +0700

    setting UI

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,21 @@ import { ProductMasterHttpService } from "./core/product/productmaster";
 import { ShopHttpService } from "./core/shop/shop";
 import { CategoryHttpService } from "./core/category/category";
 import { CutomerHttpService } from "./core/customer/customerService";
+import { SettingHttpService } from "./core/setting/setting";
 const shopHttpService = new ShopHttpService();
 const productHttpService = new ProductHttpService();
 const productMasterHttpService = new ProductMasterHttpService();
 const categoryHttpService = new CategoryHttpService();
 const customerHttpService = new CutomerHttpService();
+const settingHttpService = new SettingHttpService()
 
 const CoreAPI = {
     shopHttpService,
     productHttpService,
     productMasterHttpService,
     categoryHttpService,
-    customerHttpService
+    customerHttpService,
+    settingHttpService,
 };
 
 export default CoreAPI;
